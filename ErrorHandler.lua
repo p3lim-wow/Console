43b05fad32a40a6e241a00992887b637c238b0cc
p3lim <adrianlund@gmail.com>
2010-09-20 01:43:26 +0200

    Add a custom frame to show bugs

--- ErrorHandler.lua
+++ ErrorHandler.lua
@@ -13,31 +13,13 @@ seterrorhandler(function(error)
 	end
 end)
 
-local function Popup(text)
-	local dialog = StaticPopup_Show('Console')
-	if(dialog) then
-		dialog.editBox:SetText(text)
-		dialog.editBox:SetFocus()
-		dialog.editBox:HighlightText()
-	end
-end
-
 SLASH_Console1 = '/bugs'
 SlashCmdList.Console = function(str)
 	if(BUGS[tonumber(str)]) then
-		Popup(BUGS[tonumber(str)])
+		_G['ConsoleScrollBox']:SetText(BUGS[tonumber(str)])
 	elseif(#BUGS ~= 0) then
-		Popup(BUGS[#BUGS])
+		_G['ConsoleScrollBox']:SetText(BUGS[#BUGS])
 	else
 		print('|cffff8080Console:|r Na na na na na na BATMAN!')
 	end
 end
-
-StaticPopupDialogs.Console = {
-	text = 'Copy to Clipboard',
-	EditBoxOnEscapePressed = function(self) self:GetParent():Hide() end,
-	EditBoxOnEnterPressed = function(self) self:GetParent():Hide() end,
-	hasEditBox = 1,
-	whileDead = 1,
-	timeout = 0
-}
